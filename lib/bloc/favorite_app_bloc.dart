71c0f78e5c5023191a2143a45841365df3ebf571
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-03-02 12:56:17 +0500

    bloc state created

--- lib/bloc/favorite_app_bloc.dart
+++ lib/bloc/favorite_app_bloc.dart
@@ -1,13 +1,1 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
 
-part 'favorite_app_event.dart';
-part 'favorite_app_state.dart';
-
-class FavoriteAppBloc extends Bloc<FavoriteAppEvent, FavoriteAppState> {
-  FavoriteAppBloc() : super(FavoriteAppInitial()) {
-    on<FavoriteAppEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
-}
--- lib/bloc/favorite_app_state.dart
+++ lib/bloc/favorite_app_state.dart
@@ -1,10 +1,22 @@
-part of 'favorite_app_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:favorite_app/model/favorite-item-model.dart';
 
-sealed class FavoriteAppState extends Equatable {
-  const FavoriteAppState();
-  
+enum ListStatus { loading, success, failure }
+
+class FavouriteItemState extends Equatable {
+  final List<FavouriteItemModel> favouriteItemList;
+  final ListStatus listStatus;
+  const FavouriteItemState(
+      {this.favouriteItemList = const [],
+      this.listStatus = ListStatus.loading});
+
+  FavouriteItemState copyWith(
+      {List<FavouriteItemModel>? favouriteItemList, ListStatus? listStatus}) {
+    return FavouriteItemState(
+        favouriteItemList: favouriteItemList ?? this.favouriteItemList,
+        listStatus: listStatus ?? this.listStatus);
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [favouriteItemList];
 }
-
-final class FavoriteAppInitial extends FavoriteAppState {}
